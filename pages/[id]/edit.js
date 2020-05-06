e76c5d6f1d175b46c6ee41e67eecce65ee1df175
robg1638 <39894776+robg1638@users.noreply.github.com>
2020-05-05 18:13:20 -0700

    Update edit.js

--- pages/[id]/edit.js
+++ pages/[id]/edit.js
@@ -183,9 +183,9 @@ export async function getServerSideProps(context) {
   } = await requiredAuth(context);
 
   let queryIdBills = context.query.id;
+  //const res = await fetch(`http://localhost:3000/api/bills/${queryIdBills}`);
   const res = await fetch(
-  //`http://localhost:3000/api/bills/${queryIdBills}`
-  `https://cs48-s20-s1-t3-prod.herokuapp.com/api/bills/${queryIdBills}`
+    `https://cs48-s20-s1-t3-prod.herokuapp.com/api/bills/${queryIdBills}`
   );
   const { data } = await res.json();
   return { props: { bills: data, user: user } };
