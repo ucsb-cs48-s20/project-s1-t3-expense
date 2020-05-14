cb9e7ec4c593b1c26501e6313f4b771072d8644f
Kobe Shavolian <kobeshavolian@yahoo.com>
2020-05-14 16:19:56 -0700

    ks - changed local to prod links

--- pages/[id]/index.js
+++ pages/[id]/index.js
@@ -34,8 +34,8 @@ const Bills = ({ bills, user }) => {
     const billId = router.query.id;
     try {
       const deleted = await fetch(
-        `http://localhost:3000/api/bills/${billId}`,
-        //`https://cs48-s20-s1-t3-prod.herokuapp.com/api/bills/${billId}`,
+        //`http://localhost:3000/api/bills/${billId}`,
+        `https://cs48-s20-s1-t3-prod.herokuapp.com/api/bills/${billId}`,
         {
           method: "DELETE",
         }
@@ -101,8 +101,10 @@ export async function getServerSideProps(context) {
   } = await requiredAuth(context);
 
   let queryIdBills = context.query.id;
-  const res = await fetch(`http://localhost:3000/api/bills/${queryIdBills}`);
-  //const res = await fetch(`https://cs48-s20-s1-t3-prod.herokuapp.com/api/bills/${queryIdBills}`);
+  //const res = await fetch(`http://localhost:3000/api/bills/${queryIdBills}`);
+  const res = await fetch(
+    `https://cs48-s20-s1-t3-prod.herokuapp.com/api/bills/${queryIdBills}`
+  );
   const { data } = await res.json();
   return { props: { bills: data, user: user } };
 }
