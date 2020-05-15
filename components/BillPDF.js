fb506221194969522879defabefbaab03247d37f
gbaroni888 <gbaroni@ucsb.edu>
2020-05-15 13:43:02 -0700

    gb-Added more style to the BillPDF component

--- components/BillPDF.js
+++ components/BillPDF.js
@@ -3,26 +3,52 @@ import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
 function BillPDF(props) {
   const styles = StyleSheet.create({
     page: {
-      flexDirection: "row",
       backgroundColor: "#E4E4E4",
+      fontFamily: "Helvetica",
+      paddingTop: 30,
+      paddingLeft: 60,
+      paddingRight: 60,
+      lineHeight: 1.5,
+      flexDirection: "column",
     },
     section: {
-      margin: 10,
+      margin: 30,
       padding: 10,
       flexGrow: 1,
+      textAlign: "center",
+    },
+    date: {
+      flexDirection: "row",
+      justifyContent: "flex-end",
+      fontSize: 12,
+    },
+    body: {
+      fontSize: 16,
     },
   });
+
+  const date = new Date().toISOString().replace(/T/, " ").replace(/\..+/, "");
 
   return (
     <Document>
       <Page size="A4" style={styles.page}>
+        <View style={styles.date}>
+          <Text style={{ width: 40 }}>Date: </Text>
+          <Text>{date}</Text>
+        </View>
+
         <View style={styles.section}>
-          <Text>{props.bills.title}</Text>
-          <Text>Group Size: {props.bills.groupSize}</Text>
-          <Text>
+          <Text style={{ fontSize: 24, fontStyle: "bold" }}>
+            {props.bills.title}
+          </Text>
+
+          <Text style={styles.body}>Group Size: {props.bills.groupSize}</Text>
+
+          <Text style={styles.body}>
             Total Amount: ${JSON.parse(props.bills.dollarAmount)?.toFixed(2)}
           </Text>
-          <Text>
+
+          <Text style={styles.body}>
             To split with {props.bills.groupSize} people evenly, everyone pays:
             $
             {(
@@ -30,7 +56,8 @@ function BillPDF(props) {
               JSON.parse(props.bills.groupSize)
             ).toFixed(2)}
           </Text>
-          <Text>{props.bills.description}</Text>
+
+          <Text style={{ fontSize: 12 }}>{props.bills.description}</Text>
         </View>
       </Page>
     </Document>
--- components/ExportPDF.js
+++ components/ExportPDF.js
@@ -2,11 +2,13 @@ import { PDFDownloadLink } from "@react-pdf/renderer";
 import BillPDF from "./BillPDF";
 
 function ExportPDF(props) {
+  const title = props.bills.title + ".pdf";
+
   return (
     <div>
       <PDFDownloadLink
         document={<BillPDF bills={props.bills} />}
-        fileName="Bill.pdf"
+        fileName={title}
       >
         {({ blob, url, loading, error }) =>
           loading ? "Loading document..." : "Export as a PDF"
