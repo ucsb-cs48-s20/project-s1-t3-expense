a4250b95b8e04ab987f99d927e485b7f1b3924a9
Kaylee <kaylee@ucsb.edu>
2020-05-19 22:50:14 -0700

    kn - updated temp bill with custom calc

--- pages/new-temporary.js
+++ pages/new-temporary.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { useState, useEffect } from "react";
 import fetch from "isomorphic-unfetch";
 import Button from "react-bootstrap/Button";
-import { Form, Checkbox, Loader } from "semantic-ui-react";
+import { Form, Checkbox, Loader, Radio } from "semantic-ui-react";
 import { useRouter } from "next/router";
 import { requiredAuth } from "../utils/ssr";
 import Cookie from "js-cookie";
@@ -12,8 +12,11 @@ const NewBill = () => {
     title: "",
     description: "",
     groupSize: 1,
-    dollarAmount: 0.01,
-    members: [],
+    dollarAmount: 0,
+    remainingAmount: 0,
+    splitWay: "equal",
+    paid: false,
+    members: [{ name: "", cost: 0 }],
     // paid: false,
   });
   const [isSubmitting, setIsSubmitting] = useState(false);
@@ -31,13 +34,31 @@ const NewBill = () => {
           title: "",
           description: "",
           groupSize: 1,
-          dollarAmount: 0.01,
-          members: [],
+          dollarAmount: 0,
+          remainingAmount: 0,
+          splitWay: "equal",
+          paid: false,
+          members: [{ name: "", cost: 0 }],
           // paid: false,
         });
       }
     }
   }, [errors]);
+
+  const calculateRemainingAmount = (e) => {
+    console.log("calcualte");
+    let remainingAmount = e;
+    console.log(e);
+    form.members.forEach((member) => {
+      remainingAmount = remainingAmount - member.cost;
+    });
+    return remainingAmount;
+  };
+
+  const equalCostPerMemberString = () => {
+    const costPerMember = (form.dollarAmount / form.groupSize).toFixed(2);
+    return form.members?.length > 0 ? costPerMember : "";
+  };
 
   const createBill = async () => {
     Cookie.set("form", JSON.stringify(form));
@@ -45,33 +66,103 @@ const NewBill = () => {
   };
 
   const handleSubmit = (e) => {
+    let test = [];
+    if (form.splitWay === "equal") {
+      for (let i = 0; i < form.members.length; i++) {
+        test[i] = {
+          name: form.members[i].name,
+          cost: equalCostPerMemberString(),
+        };
+      }
+      setForm({
+        ...form,
+        members: test,
+      });
+    }
     e.preventDefault();
     let errs = validate();
     setErrors(errs);
     setIsSubmitting(true);
   };
+
+  const handleStyle = (e, { value }) => {
+    setForm({
+      ...form,
+      splitWay: value,
+    });
+  };
+
   const handleChange = (e) => {
     let test = [];
     if (e.target.name === "groupSize") {
       for (let i = 0; i < e.target.value; i++) {
-        if (form.members[i]) test[i] = form.members[i];
-        else test[i] = i.toString();
+        console.log("HELLO");
+        if (form.members[i]) {
+          test[i] = { name: form.members[i].name, cost: form.members[i].cost };
+        } else {
+          console.log("HI");
+          test[i] = { name: "", cost: 0 };
+        }
       }
     }
     if (test.length === 0) test = form.members;
-
+    //console.log(test)
     setForm({
       ...form,
       [e.target.name]: e.target.value,
       members: test,
     });
   };
+
   const handleCheck = (e) => {
     setCheck(!check);
     setForm({
       ...form,
       // ["paid"]: !check,
     });
+  };
+
+  const handleMoney = (e) => {
+    setForm({
+      ...form,
+      remainingAmount: calculateRemainingAmount(e.target.value),
+      dollarAmount: e.target.value,
+    });
+    //console.log(form.members)
+  };
+
+  const handleMemberCost = (e, index) => {
+    const newCost = e.target.value;
+
+    const newMemberList = form.members;
+    newMemberList[index] = {
+      name: newMemberList[index].name,
+      cost: e.target.value,
+    };
+
+    setForm({
+      ...form,
+      members: newMemberList,
+      remainingAmount: calculateRemainingAmount(form.dollarAmount),
+    });
+    //console.log(form.members)
+  };
+
+  const handleMemberName = (e, index) => {
+    const newName = e.target.value;
+
+    const memberObject = form.members;
+
+    memberObject[index] = {
+      name: e.target.value,
+      cost: memberObject[index].cost,
+    };
+
+    setForm({
+      ...form,
+      members: memberObject,
+    });
+    //console.log(form.members)
   };
 
   const validate = () => {
@@ -88,16 +179,6 @@ const NewBill = () => {
       err.description = "Description must be less taht 200 characters";
     }
     return err;
-  };
-
-  const handleMem = (e) => {
-    let test = form.members;
-    test[e.target.placeholder] = e.target.value;
-    console.log(test);
-    setForm({
-      ...form,
-      members: test,
-    });
   };
 
   return (
@@ -137,29 +218,73 @@ const NewBill = () => {
 
             <div className="mem-indent">
               {form.members?.map((item, index) => {
-                //console.log(index)
                 return (
-                  <Form.Input
-                    key={index}
-                    fluid
-                    label="Member"
-                    placeholder={index}
-                    name="members"
-                    onChange={handleMem}
-                  />
+                  <div>
+                    <Form.Input
+                      key={index}
+                      fluid
+                      label="Member Name"
+                      placeholder={index + 1}
+                      name="members"
+                      onChange={(e) => {
+                        handleMemberName(e, index);
+                      }}
+                    />
+                    {form.splitWay === "equal" ? (
+                      equalCostPerMemberString()
+                    ) : (
+                      <div>
+                        <Form.Input
+                          name="expense"
+                          type="number"
+                          step="1"
+                          min="0"
+                          onChange={(e) => {
+                            handleMemberCost(e, index);
+                          }}
+                        />
+                      </div>
+                    )}
+                  </div>
                 );
               })}
             </div>
+            {form.splitWay === "equal" ? (
+              <p>Remaining Balance: 0</p>
+            ) : (
+              <p>Remaining Balance: {form.remainingAmount}</p>
+            )}
 
             <Form.Input
-              label="Amount"
+              label="Total Amount"
               placeholder="Enter amount"
               name="dollarAmount"
               type="number"
               step="0.01"
               min="0.01"
-              onChange={handleChange}
+              value={form.dollarAmount}
+              onChange={handleMoney}
             />
+
+            <Form.Group inline>
+              <label>Split Method</label>
+              <Form.Field
+                name="splitWay"
+                control={Radio}
+                label="Equal"
+                value="equal"
+                checked={form.splitWay === "equal"}
+                onChange={handleStyle}
+              />
+              <Form.Field
+                name="splitWay"
+                control={Radio}
+                label="Custom"
+                value="custom"
+                checked={form.splitWay === "custom"}
+                onChange={handleStyle}
+              />
+            </Form.Group>
 
             <Form.TextArea
               fluid
