c3ba2ef44d92fdd5abda1e03fa7357318cc73380
Dylan Lin <46723393+DylanLin29@users.noreply.github.com>
2020-05-28 14:11:50 -0700

    update unit test

---
+++ __tests__/validateForm.js
@@ -0,0 +1,36 @@
+import { validateForm } from "../utils/validateForm";
+
+describe("util/validateForm", () => {
+  describe("validateForm", () => {
+    it("returns an empty object", () => {
+      expect(validateForm("Grocery", "Apple and Banana")).toEqual({});
+    });
+    it("returns an object with error message when first parameter is empty", () => {
+      expect(validateForm("", "Apple and Banana")).toEqual({
+        title: "Title is required",
+      });
+    });
+    it("returns an object with error message when second parameter is empty", () => {
+      expect(validateForm("Grocery", "")).toEqual({
+        description: "Description is required",
+      });
+    });
+    it("returns an object with error messaage when the second parameter's length > 200", () => {
+      expect(
+        validateForm(
+          "Grocery",
+          "Apple, Banana, Strawberry, Apple, Banana, Strawberry, Apple, Banana, Strawberry, " +
+            "Apple, Banana, Strawberry, Apple, Banana, Strawberry, Apple, Banana, Strawberry, " +
+            "Apple, Banana, Strawberry, Apple, Banana "
+        )
+      ).toEqual({
+        description: "Description must be less than 200 characters",
+      });
+    });
+    it("returns an object with error message when the first parameter's length > 40", () => {
+      expect(
+        validateForm("Grocery, Grocery, Grocery, Grocery, Grocery", "Apple")
+      ).toEqual({ title: "Title must be less than 40 characters" });
+    });
+  });
+});
---
+++ utils/calculations.js
@@ -0,0 +1,11 @@
+export const equalCostPerMemberString = (dollarAmount, groupSize) => {
+  const costPerMember = (dollarAmount / groupSize).toFixed(2);
+  return groupSize > 0 ? costPerMember : "";
+};
+
+export const calculateRemainingAmount = (remainingAmount, members) => {
+  members.forEach((member) => {
+    remainingAmount = remainingAmount - member.cost;
+  });
+  return remainingAmount;
+};
