147f9c3c812a76b0c3ebbbb0d13938115982ddcb
Kobe Shavolian <kobeshavolian@yahoo.com>
2020-05-29 17:17:39 -0700

    Fixed .toFixed(2) test error

--- __tests__/calculateRemainingAmount.js
+++ __tests__/calculateRemainingAmount.js
@@ -4,7 +4,7 @@ describe("util/calculations", () => {
   describe("calculateRemainingAmount", () => {
     it("calculate the remaining amount after subtracting the cost of each member", () => {
       const members = [{ cost: 2 }, { cost: 5 }];
-      expect(calculateRemainingAmount(10, members)).toBe(3);
+      expect(calculateRemainingAmount(10, members)).toBe(3.0);
     });
   });
 });
