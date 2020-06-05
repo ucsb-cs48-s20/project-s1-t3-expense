e42cb54d364768513dd0711390a4320d2c2a88e1
robg1638 <rgutierrez555@umail.ucsb.edu>
2020-06-05 01:36:00 -0700

    rg - Finalized look of home page

--- components/AppJumbotron.js
+++ components/AppJumbotron.js
@@ -8,34 +8,32 @@ function AppJumbotron(props) {
   return (
     <Container>
       <Jumbotron fluid variant="" className="Jumbotron">
-        <h1 className="myfont">Housing Expenses Splitter</h1>
-        <p className="styles.fontColor">
-          A web application to keep track of bills and owed money, can support
-          payments such as rent or one time fees. Users can choose to split the
-          bills evenly or customize by themselves.
-        </p>
+        <h1 className="jumboFont">Housing Expenses Splitter</h1>
+        <p className="jumboText">A web application to keep track of bills</p>
       </Jumbotron>
       <div className="container">
         <div className="row">
           <div className="col-sm-4">
-            <h3>Create And Edit Bills</h3>
+            <h3>Create, Edit, And Save Bills</h3>
             <img src="/images/bill.png" alt="Bill" className="center" />
             <p className="center-text">
-              To keep track of whos paying who as expenses pile up
+              Create a quick temporary bill to sort out whos paying who fast, or
+              log in to create multiple bills that are saved onto your account
             </p>
           </div>
           <div className="col-sm-4">
             <h3>Invite Others To Bills</h3>
             <img src="/images/members.png" alt="Members" className="center" />
             <p className="center-text">
-              To keep everyone posted on whos paying what
+              If you're logged in, invite others to your bill so everyone is on
+              the same page
             </p>
           </div>
           <div className="col-sm-4">
             <h3>Export Reciept PDFs</h3>
             <img src="/images/pdflogo.png" alt="PDFlogo" className="center" />
             <p className="center-text">
-              To keep a nice summary of all your bills
+              For when you need a nice summary of your bill
             </p>
           </div>
         </div>
